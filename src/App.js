edcd08921ece521048e584db16244366661605e1
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-06-18 00:26:05 -0300

    basic layout

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ExerciseDetail from './pages/ExerciseDetail';
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
