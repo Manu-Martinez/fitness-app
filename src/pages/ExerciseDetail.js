25bc7c740b85f8a53c83703954749a670c8dad36
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-06-20 00:29:52 -0300

    fixed issue related to fetching videos

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
-import { exerciseOptions, fetchData } from "../utils/fetchData";
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
 
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
@@ -11,15 +11,20 @@ import SimilarExercises from '../components/SimilarExercises';
 const ExerciseDetail = () => {
 
     const [exerciseDetail, setExerciseDetail] = useState({});
+	const [exerciseVideos, setExerciseVideos] = useState([]);
 	const { id } = useParams();
 
 	useEffect(() => {
 		const fetchExercisesData = async () => {
 			const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
 			const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
-			const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
 
+
+			const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
 			setExerciseDetail(exerciseDetailData);
+
+			const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions);
+			setExerciseVideos(exerciseVideosData);
 		} 
 
 		fetchExercisesData();
@@ -28,7 +33,7 @@ const ExerciseDetail = () => {
     return (
     <Box>
 		<Detail exerciseDetail={exerciseDetail}/>
-		<ExerciseVideos />
+		<ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
 		<SimilarExercises />
 	</Box>
     );
