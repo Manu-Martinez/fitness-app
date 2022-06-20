abd2f3f44b22b7bcf44e0e6eb978c321571d7c35
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-06-20 00:02:20 -0300

    added youtube-search api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,10 +1,17 @@
 export const exerciseOptions = {
 	method: 'GET',
-	url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
 	headers: {
-    'X-RapidAPI-Key': process.env.REACT_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-}
+	'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_RAPID_API_KEY
+    },
+};
+
+const options = {
+	method: 'GET',
+	headers: {
+	'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+	'X-RapidAPI-Key': process.env.REACT_RAPID_API_KEY
+	},
 };
 
 
